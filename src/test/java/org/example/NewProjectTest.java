851cf6d658f1c05c0632b23a99b116c734653f02
vserij2013 <v.serij2013@yandex.ru>
2021-12-20 10:59:26 +1000

    Initial commit.

--- src/test/java/org/example/NewProjectTest.java
+++ src/test/java/org/example/NewProjectTest.java
@@ -8,7 +8,7 @@ public class NewProjectTest extends AbstractTest {
     @Test
     void CreateProject() {
         new DiaryMainPage(getDriver()).goToNewProjectPage();
-        Assertions.assertTrue(driver.getTitle().equals("Регистрация — @дневники: асоциальная сеть"), "страница опубликования недоступна");
+        Assertions.assertTrue(driver.getTitle().equals("Создание записи — @дневники: асоциальная сеть"), "страница опубликования недоступна");
         new NewProjectPage(getDriver()).setSubject("453353453").setTheme().CreateProject();
     }
 
